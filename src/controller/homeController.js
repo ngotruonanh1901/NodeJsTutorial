c604b3b8f63b61b58af07ea2d3f8bea3f28ebb7e
ntanh1101 <ngotruonganh1901@gmail.com>
2022-11-18 21:57:44 +0700

    Add DB

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,17 @@
-let getHomepage = (req, res) => {
-  return res.render("index.ejs");
+import connection from "../configs/connectDB";
+
+let getHomepage = async (req, res) => {
+  let data = [];
+  connection.query("SELECT * FROM `users`", function (err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    results.map((row) => {
+      data.push({ id: row.id, name: row.name });
+    });
+    return res.render("index.ejs", { dataUser: JSON.stringify(data) });
+  });
+  const [rows, fields ] = await connection.execute("select * From `user`")
 };
+
 
 module.exports = {
   getHomepage,
--- src/route/web.js
+++ src/route/web.js
@@ -1,14 +1,13 @@
 import express from "express";
-import homeController from "../controller/homeController"
-import Controller from "../controller/homeController"
+import homeController from "../controller/homeController";
 let router = express.Router();
 
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomepage);
 
   router.get("/about", (req, res) => {
-      res.send(`about page`);
-    });
+    res.send(`about page`);
+  });
 
   return app.use("/", router);
 };

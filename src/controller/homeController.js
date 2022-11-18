8a1ca3732adfb8e1dde89d837e5ff56897dde10d
ntanh1101 <ngotruonganh1901@gmail.com>
2022-11-18 22:08:13 +0700

    Add Navbar

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,17 +1,15 @@
 import connection from "../configs/connectDB";
 
-let getHomepage = async (req, res) => {
+let getHomepage = (req, res) => {
   let data = [];
   connection.query("SELECT * FROM `users`", function (err, results, fields) {
-    console.log(results); // results contains rows returned by server
     results.map((row) => {
       data.push({ id: row.id, name: row.name });
     });
     return res.render("index.ejs", { dataUser: JSON.stringify(data) });
   });
-  const [rows, fields ] = await connection.execute("select * From `user`")
+//   const [rows, fields] = await connection.execute("select * From `user`");
 };
-
 
 module.exports = {
   getHomepage,

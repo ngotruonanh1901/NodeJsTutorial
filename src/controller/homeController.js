e9c916b9971a980444b09c4e6e29974ae0061420
ntanh1101 <ngotruonganh1901@gmail.com>
2022-11-20 23:30:58 +0700

    Changed DB Success

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,37 +1,49 @@
 import pool from "../configs/connectDB";
 
+// get all user
 let getHomepage = async (req, res) => {
-  const [rows, fields] = await pool.execute("SELECT * FROM `users`");
+  const [rows, fields] = await pool.execute("SELECT * FROM user");
   return res.render("index.ejs", { dataUser: rows });
 };
 
+// get detail user
 let getDetailPage = async (req, res) => {
   let userId = req.params.id;
-  let [user] = await pool.execute("SELECT * FROM USERS WHERE id = ?", [userId]);
-  return res.send(user);
+  let [user] = await pool.execute("SELECT * FROM user WHERE id = ?", [userId]);
+  return res.render("detail.ejs", { dataUser: user[0] });
 };
 
+// create user
 let createNewUser = async (req, res) => {
-  let { lastName } = req.body;
-  await pool.execute("INSERT INTO users(name) values (?)", [lastName]);
+  let { email, password } = req.body;
+  await pool.execute("INSERT INTO user(email, password) values (?, ?)", [
+    email,
+    password,
+  ]);
   return res.redirect("/");
 };
 
+// delete user
 let deleteUser = async (req, res) => {
   let userId = req.body.userId;
-  await pool.execute("DELETE FROM users WHERE id = ?", [userId]);
+  await pool.execute("DELETE FROM user WHERE id = ?", [userId]);
   return res.redirect("/");
 };
 
+// edit user
 let getEditPage = async (req, res) => {
-  let id = req.params.id;
-  let [user] = await pool.execute("SELECT * FROM USERS WHERE id = ?", [id]);
+  let userId = req.params.id;
+  let [user] = await pool.execute("SELECT * FROM user WHERE id = ?", [userId]);
   return res.render("update.ejs", { dataUser: user[0] });
 };
 
 let postEditPage = async (req, res) => {
-  let { id, lastName } = req.body;
-  await pool.execute("UPDATE USERS SET name = ? WHERE id = ?", [lastName, id]);
+  let { id, email, password } = req.body;
+  await pool.execute("UPDATE user SET email = ?, password = ?  WHERE id = ?", [
+    email,
+    password,
+    id,
+  ]);
   return res.redirect("/");
 };
 

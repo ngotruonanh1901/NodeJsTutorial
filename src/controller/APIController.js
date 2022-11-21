e402c1fb349adccf5b92755c2c6d24b974c3a4cc
ntanh1101 <ngotruonganh1901@gmail.com>
2022-11-21 11:13:07 +0700

    Done CRUD API

--- src/controller/APIController.js
+++ src/controller/APIController.js
@@ -1,72 +1,77 @@
 import pool from "../configs/connectDB";
 
 let getAllUser = async (req, res) => {
-  const [rows, fields] = await pool.execute("SELECT * FROM users");
-
+  const [rows, fields] = await pool.execute("SELECT * FROM user");
   return res.status(200).json({
     data: rows,
   });
 };
 
+// create new user
 let createNewUser = async (req, res) => {
-  let { lastName } = req.body;
-  if (!lastName) {
-    return res.status(200).json({
-      message: "missing",
+  let { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(500).json({
+      message: "Missing",
     });
   }
-
-  await pool.execute("INSERT INTO users(name) values (?)", [lastName]);
+  await pool.execute("INSERT INTO user(email, password) values (?, ?)", [
+    email,
+    password,
+  ]);
   return res.status(200).json({
-    message: "ok",
+    message: "Ok",
   });
 };
 
+// deleteUser
 let deleteUser = async (req, res) => {
   let userId = req.params.id;
   if (!userId) {
-    return res.status(200).json({
-      message: "missing",
+    return res.status(500).json({
+      message: "Missing",
     });
   }
-
-  await pool.execute("DELETE FROM users WHERE id = ?", [userId]);
+  await pool.execute("DELETE FROM user WHERE id = ?", [userId]);
   return res.status(200).json({
-    message: "ok",
+    message: "Ok",
   });
 };
 
+// update user
 let updateUser = async (req, res) => {
-  let { id, lastName } = req.body;
-  if (!lastName || !id) {
-    return res.status(200).json({
-      message: "missing",
+  let { id, email, password } = req.body;
+  if (!id || !email || !password) {
+    return res.status(500).json({
+      message: "Missing",
     });
   }
-
-  await pool.execute("UPDATE USERS SET name = ? WHERE id = ?", [lastName, id]);
+  await pool.execute("UPDATE user SET email = ?, password = ? WHERE id = ?", [
+    email,
+    password,
+    id,
+  ]);
   return res.status(200).json({
-    message: "ok",
+    message: "Ok",
   });
 };
+
 
 let login = (req, res) => {
-  let email = req.body.email;
-  let password = req.body.password;
-
+  let { email, password } = req.body;
   if (!email || !password) {
     return (
       res.status(500),
       json({
         errCode: 1,
-        message: "missing input",
+        message: "missing",
       })
     );
   }
 
   return res.status(200).json({
     errCode: 0,
-    message: "success",
+    message: "Ok",
   });
 };
 
